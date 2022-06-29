c5e6432972fc221caf420c657f08a1e56a462cdc
Casper Bruning <82331679+MagicGnome-UwU@users.noreply.github.com>
2022-06-29 14:19:26 +0200

    removed useless files / changed dir name

--- statements_3.php
+++
@@ -1,8 +0,0 @@
-<?php
-$geslacht = "man";
-
-if($geslacht == "man"){
-    echo "Je gaat naar een voetbalwedstrijd";
-} elseif($geslacht =="vrouw") {
-    echo "Je gaat een dagje shoppen";
-}
--- webdevellopment/PupperWear.com/public/js/js.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(`#product11`).addEventListener("mouseenter", function() {
-    document.querySelector(`#product11`).style.filter = "blur(10px)";
-
-})
-
-let getal1 = 1;
-console.log(getal1)
